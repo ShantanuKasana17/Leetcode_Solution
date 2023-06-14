8639d9feb709c42622b4804cb16f2c8885a50aa2
Shantanu Kasana <98523819+ShantanuKasana17@users.noreply.github.com>
2023-06-14 10:20:57 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (84.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,13 +5,13 @@ public:
         int count1 = 0;
         int count2 = 0;
         for(int i = 0;i<nums.size();i++){
-            if(nums[i]==0) count0+=1;
-            else if(nums[i]==1) count1+=1;
-            else count2+=1;
+            if(nums[i]==0) count0 += 1;
+            else if(nums[i]==1) count1 +=1;
+            else count2 +=1;
         }
-        for(int i = 0;i<count0;i++)  nums[i]=0;
-        for(int i = count0;i<count0+count1;i++) nums[i]=1;
-        for(int i = count0+count1;i<nums.size();i++)  nums[i]=2;
-        return; 
-    }
+        for(int i = 0;i<count0;i++) nums[i] = 0;
+        for(int i = count0;i<count0+count1;i++) nums[i] = 1;
+        for(int i = count0+count1;i<nums.size();i++) nums[i] = 2;
+        return;
+        }
 };
