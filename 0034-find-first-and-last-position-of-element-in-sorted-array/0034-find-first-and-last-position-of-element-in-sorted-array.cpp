f17eec7b7d2296219705c3e04dc9e1a21a26ebaa
Shantanu Kasana <98523819+ShantanuKasana17@users.noreply.github.com>
2023-07-03 00:38:22 +0530

    Time: 17 ms (6.51%), Space: 13.5 MB (97.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,41 @@
 class Solution {
 private: 
     int firstOcc(vector<int>& nums, int target){
-        int start = 0 ;
-        int end = nums.size()-1;
+        int n = nums.size();
+        int low = 0 ;
+        int high = n-1;
         int ans = -1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
             if(nums[mid] == target){
                 ans = mid;
-                end = mid - 1;
+                high = mid - 1;
             }
             else if(nums[mid] < target){
-                start = mid+1;
+                low = mid+1;
             }
             else{
-                end = mid - 1;
+                high = mid - 1;
             }
         }
         return ans;
     }
     int SecondOcc(vector<int>& nums, int target){
-        int start = 0 ;
-        int end = nums.size()-1;
+        int n = nums.size();
+        int low = 0 ;
+        int high = n-1;
         int ans = -1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
             if(nums[mid] == target){
                 ans = mid;
-                start = mid + 1;
+                low = mid + 1;
             }
             else if(nums[mid] < target){
-                start = mid+1;
+                low = mid+1;
             }
             else{
-                end = mid - 1;
+                high = mid - 1;
             }
         }
         return ans;
