90c3578362fa206580880326d0971b3caf3a4df7
Shantanu Kasana <98523819+ShantanuKasana17@users.noreply.github.com>
2023-07-27 16:40:46 +0530

    Time: 177 ms (99.47%), Space: 74 MB (43.47%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,14 +9,16 @@
  * };
  */
 
-/*
+
 class Solution {
 public:
+    
+     // Middle of the ll
     ListNode * Mid(ListNode * head){
         ListNode * slow = head;
         ListNode * fast = head;
         
-        while(fast && fast -> next)
+        while(fast -> next && fast -> next -> next)
         {
             slow = slow -> next;
             fast = fast -> next -> next;
@@ -24,13 +26,15 @@ public:
         return slow;
         
     }
+
+     //  merging sorted list
     ListNode * mergeSortedList(ListNode * p1,ListNode * p2)
     {
         if(p1 == NULL || p2 == NULL)
         {
             return (p1 == NULL) ? p2:p1;
         }
-        ListNode * ans = new ListNode();
+        ListNode * ans = new ListNode(0);
         ListNode * curr = ans;
         
         while(p1 != NULL  && p2 != NULL)
@@ -53,7 +57,7 @@ public:
             }
             return ans -> next;
     }
-    
+    // sortedlist 
     ListNode* sortList(ListNode* head) {
         if(head == NULL || head ->next == NULL) return head;
         ListNode * mid = Mid(head);
@@ -65,59 +69,6 @@ public:
     
     return mergeSortedList(left_half,right_half);
     }
-    
 };
-*/
 
-class Solution {
-public:
-    
-    ListNode* Mid(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(fast->next != NULL and fast->next->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-    
-    ListNode* mergeSortedList(ListNode *p1, ListNode *p2){
-        if(p1 == NULL or p2 == NULL){
-            return (p1==NULL) ? p2 : p1;
-        }
-        
-        ListNode * ans = new ListNode(0);
-        ListNode* curr = ans;
-        
-        while(p1 != NULL and p2 != NULL){
-            if(p1->val < p2->val){
-                curr->next = p1;
-                p1= p1->next;
-            }else{
-                curr->next = p2;
-                p2 = p2->next;
-            }
-            curr = curr->next;
-        }
-        
-        if(p1 != NULL or p2 != NULL){
-            curr->next = (p1 != NULL) ?  p1 : p2;
-        }
-        return ans->next;
-    }
-    
-    ListNode* sortList(ListNode* head) {
-        if( head == NULL or head->next == NULL) return head;
-        ListNode* mid = Mid(head);
-        ListNode* newhead = mid->next;
-        mid->next = NULL;
-        
-        ListNode* left_half = sortList(head);
-        ListNode* right_half = sortList(newhead);
-        
-        return mergeSortedList(left_half, right_half);
-    }
-};
 
