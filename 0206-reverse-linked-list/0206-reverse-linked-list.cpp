4f9f53193dc332ffab0e9c99b93c48423983addf
Shantanu Kasana <98523819+ShantanuKasana17@users.noreply.github.com>
2023-07-29 11:59:04 +0530

    Time: 8 ms (32.26%), Space: 8.3 MB (25.84%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,20 +11,15 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode * prev_p = NULL;
-        ListNode * curr_p = head;
-        ListNode * next_p;
-        
-        while(curr_p)
+        ListNode *  newhead = NULL;
+        while(head != NULL)
         {
-            next_p = curr_p -> next; // immediate next of curr pointer
-            curr_p -> next = prev_p; // curr pointer next stores address of prev pointer
-            
-            prev_p = curr_p; // move prev pointer to curr pointer
-            curr_p = next_p; // move curr pointer to next 
+            ListNode * next = head -> next;
+            head -> next = newhead;
+            newhead = head;
+            head = next;
             
         }
-        head = prev_p; // moe head to prev pointer
-        return head;  // return head 
+        return newhead;
     }
 };
